f5ea76cfab724611f54667ecea14959c8122df74
Juanma-05 <j.sancho@alumno.etec.um.edu.ar>
2024-05-17 09:48:04 -0300

    tablas creadas

---
+++ init.sql
@@ -0,0 +1,18 @@
+-- Crear la tabla 'Ids' para generar IDs autoincrementales
+CREATE TABLE Ids (
+    id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+-- Crear la tabla 'Productos'
+CREATE TABLE Productos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255),
+    descripcion TEXT
+);
+
+-- Crear la tabla 'Precios'
+CREATE TABLE Precios (
+    producto_id INT NOT NULL,
+    precio DECIMAL(10,2),
+    FOREIGN KEY (producto_id) REFERENCES Productos(id)
+);
