38028146d9ec9540c8f162013a6f750c98c7fc08
Juanma-05 <j.sancho@alumno.etec.um.edu.ar>
2024-06-27 16:57:18 -0300

    Inicio de sesion agregado

--- index.js
+++ index.js
@@ -34,23 +34,64 @@ app.get("/", (req, res)=>{
     res.render('register');
 });
 
-app.post("/validar",(req, res)=>{
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post("/register",(req, res)=>{
     const datos = req.body
 
     console.log(datos);
     let gmail = datos.gmail;
     let contraseña = datos.contraseña;
 
-    let register = "INSERT INTO tabla_usuario(Gamil, Contraseña) VALUES ('"+gmail+"', '"+contraseña+"')";
-    pool.query(register, function(error){
+    let buscar = "SELECT * FROM tabla_usuario WHERE Gmail = "+gmail+"";
+    conexion.query(buscar, function(error, row){
         if (error){
             throw error;
         }else {
-            console.log("Datos guardados correctamete")
+            if (row.length>0){
+                console.log("No se puede registrar usuario ya existe")
+            }else{
+                let register = "INSERT INTO tabla_usuario(Gamil, Contraseña) VALUES ('"+gmail+"', '"+contraseña+"')";
+                pool.query(register, function(error){
+                    if (error){
+                        throw error;
+                    }else {
+                        console.log("Datos guardados correctamete")
+                    }
+                });
+                
+            }
         }
-    })  
+    })
+
+  
 });
 
+app.post('/login', async (req, res) => {
+    const { gmail, contraseña } = req.body;
+    try {
+        // Buscar el usuario por su Gmail
+        const [rows] = await pool.query('SELECT * FROM tabla_usuario WHERE Gmail = ?', [gmail]);
+        if (rows.length === 0) {
+            return res.status(400).json({ message: 'Usuario o contraseña incorrectos' });
+        }
+
+        const user = rows[0];
+
+        // Comparar la contraseña encriptada
+        const isMatch = await bcrypt.compare(contraseña, user.Contraseña);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Usuario o contraseña incorrectos' });
+        }
+
+        res.json({ message: 'Inicio de sesión exitoso' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en la base de datos' });
+    }
+});
 
 app.get("/ping", async(req, res)=>{
     const result = await pool.query("SELECT NOW()")
